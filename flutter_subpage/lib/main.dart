9ba00d438cf5a864f16e44d6d4b16c616ba7e1ca
ehs2803 <hr2803@naver.com>
2021-09-05 20:34:11 +0900

    20210905

---
+++ flutter_subpage/lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'subpage',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: FirstPage(),
+    );
+  }
+}
+
+class FirstPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _FirstPage();
+}
+
+class _FirstPage extends State<FirstPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sub page Main'),
+      ),
+      body: Container(
+        child: Center(
+          child: Text('첫번재 페이지'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context)
+              .push(MaterialPageRoute(builder: (context) => SecondPage()));
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Page'),
+      ),
+      body: Container(
+        child: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text('돌아가기'),
+          ),
+        ),
+      ),
+    );
+  }
+}
