677a96ebbefbe8a77a1ac8acc5c40fa1c7727295
unknown <32022031@cuckoo.domain>
2023-03-27 11:08:15 +0900

    study_dart : start

---
+++ Dart/DartLanguageStudy_codeFactory/basic/02_variable.dart
@@ -0,0 +1,16 @@
+void main(){
+  // variable
+  var name = 'name1';
+
+  print(name);
+
+  var name2 = 'name2';
+
+  print(name2);
+
+  name = '플러터 프로그래밍';
+
+  print(name);
+
+  //var name = 'error';
+}
